aa98ed30810a9e5c2a23c191195913db19fa5ccf
Chris Thoung <chris.thoung@gmail.com>
2014-04-19 13:52:04 +0100

    Create FSIC package folder and add setup script
    Create new fsic package folder containing __init__.py.
    
    Create new setup.py script to install package.

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+from distutils.core import setup
+
+
+setup(
+    name='fsic',
+    version='0.0.0',
+    description='Tools for Stock-Flow Consistent macroeconomic modelling',
+    license='BSD',
+    author='Chris Thoung',
+    author_email='chris.thoung@gmail.com',
+    url='https://github.com/cthoung/fsic',
+    packages=['fsic'],
+    )
