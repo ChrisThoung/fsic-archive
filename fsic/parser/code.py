eff2e514337806c0f9b621794ecb1bd27f37f3ef
Chris Thoung <chris.thoung@gmail.com>
2014-07-20 17:08:22 +0100

    Initial implementation of lags code
    Implemented series of regular expressions to handle lags.
    
    New tests to accompany new code.
    
    Clean-up, along with leads treatment, to follow.

--- fsic/parser/code.py
+++ fsic/parser/code.py
@@ -27,9 +27,9 @@ def translate(block, period='period'):
 
     Notes
     =====
-    The purpose of this function is to translate more basic Python expressions
-    (e.g. from a Pandoc Markdown file) into ones that can be included in a
-    Python class definition (i.e. a model class object). This consists of:
+    This function is to translates more basic Python expressions (e.g. from a
+    Pandoc Markdown file) into ones that can be included in a Python class
+    definition (i.e. a model class object). This consists of:
 
     * Adding 'self.' as a prefix
     * Adding a period index as a suffix (accounting for leads and lags where
@@ -46,15 +46,66 @@ def translate(block, period='period'):
     'self.G_s[time] = self.G_d[time]'
 
     """
-    pattern = re.compile(
+    # Remove unnecessary whitespace
+    while('  ' in block):
+        block = block.replace('  ', ' ')
+    split_pattern = re.compile(
+        r'''\b              # Open word boundary
+            ([A-z_]+[\w]*)  # Valid Python identifier (starts with a letter or
+                            # underscore, then any sequence of alphanumeric
+                            # characters)
+            \b              # Close word boundary
+            \s              # Single whitespace character (having already
+                            # removed all double instances)
+            \[              # Open square (index) bracket
+        ''',
+        re.VERBOSE)
+    block = split_pattern.sub(r'\1' + '[', block)
+    # Prefix pattern to add 'self.'
+    prefix_pattern = re.compile(
         r'''(               # New group
             \b              # Open word boundary
-            [A-z_]+[\w]*    # Valid Python identifier (starts with a letter or
-                            # underscore, then any sequence of alphanumeric
-                            #  characters)
+            [A-z_]+[\w]*    # Valid Python identifier
             \b              # Close word boundary
             )               # Close group
         ''',
         re.VERBOSE)
-    block = pattern.sub('self.' + r'\1'+ '[' + period + ']', block)
+    block = prefix_pattern.sub('self.' + r'\1', block)
+    # Index pattern for variables without a period index
+    index_pattern = re.compile(
+        r'''(                   # New group
+            \b                  # Open word boundary
+            self\.[A-z_]+[\w]*  # Valid Python identifier preceded by 'self.'
+            \b                  # Close word boundary
+            )                   # Close group
+            [^[]                # Next character is *not* an opening square
+                                # bracket
+        ''',
+        re.VERBOSE)
+    block = index_pattern.sub(r'\1'.strip() + '[' + period + '] ', block)
+    block = block.strip()
+    # Index pattern for the last variable in each expression
+    index_pattern_last = re.compile(
+        r'''(                   # New group
+            \b                  # Open word boundary
+            self\.[A-z_]+[\w]*  # Valid Python identifier preceded by 'self.'
+            $                   # End of string
+            )                   # Close group
+        ''',
+        re.VERBOSE)
+    block = index_pattern_last.sub(r'\1'.strip() + '[' + period + '] ', block)
+    block = block.strip()
+    # Insert pattern for variables with a lead/lag offset
+    lead_lag_pattern = re.compile(
+        r'''\[      # Open square (index) bracket
+            \s*     # Any whitespace before
+            (       # Open group
+            [\d+-]+ # One or more digits, + or -
+            )       # Close group
+            \s*     # Any whitespace after
+            \]      # Close square (index) bracket
+        ''',
+        re.VERBOSE)
+    block = lead_lag_pattern.sub('[' + period + r'\1' + ']', block)
+    # Return
     return block
--- fsic/parser/tests/test_code.py
+++ fsic/parser/tests/test_code.py
@@ -4,16 +4,48 @@
 import fsic.parser.code
 
 
-def test_one_line_no_lags():
+def test_one_line_no_lag():
     block = 'Y = W * N_d'
     assert fsic.parser.code.translate(block) == (
         'self.Y[period] = self.W[period] * self.N_d[period]')
 
 
-def test_one_line_no_lags_custom_period():
+def test_one_line_no_lag_custom_period():
     block = 'T_s = T_d'
     assert fsic.parser.code.translate(block, period='time') == (
         'self.T_s[time] = self.T_d[time]')
+
+
+def test_one_line_lag():
+    block = 'C_d = alpha_1 * YD + alpha_2 * H_h[-1]'
+    assert fsic.parser.code.translate(block) == (
+        'self.C_d[period] = '
+        'self.alpha_1[period] * self.YD[period] + '
+        'self.alpha_2[period] * self.H_h[period-1]')
+
+
+def test_one_line_lag_single_space():
+    block = 'C_d = alpha_1 * YD + alpha_2 * H_h [-1]'
+    assert fsic.parser.code.translate(block) == (
+        'self.C_d[period] = '
+        'self.alpha_1[period] * self.YD[period] + '
+        'self.alpha_2[period] * self.H_h[period-1]')
+
+
+def test_one_line_lag_multiple_spaces():
+    block = 'C_d = alpha_1 * YD + alpha_2 * H_h  [-1]'
+    assert fsic.parser.code.translate(block) == (
+        'self.C_d[period] = '
+        'self.alpha_1[period] * self.YD[period] + '
+        'self.alpha_2[period] * self.H_h[period-1]')
+
+
+def test_one_line_lag_inner_space():
+    block = 'C_d = alpha_1 * YD + alpha_2 * H_h[ -1 ]'
+    assert fsic.parser.code.translate(block) == (
+        'self.C_d[period] = '
+        'self.alpha_1[period] * self.YD[period] + '
+        'self.alpha_2[period] * self.H_h[period-1]')
 
 
 if __name__ == '__main__':
