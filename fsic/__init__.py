f064238aff74726741f42882efab6cd9172c41f7
Chris Thoung <chris.thoung@gmail.com>
2014-07-24 23:00:03 +0100

    Add version information to fsic/__init__.py
    Copy version information from setup.py to fsic/__init__.py.
    
    Investigate options to prevent duplication of variables at a later date.

--- fsic/__init__.py
+++ fsic/__init__.py
@@ -7,3 +7,14 @@ specification, solution and analysis of Stock-Flow Consistent macroeconomic
 models in the tradition of Wynne Godley.
 
 """
+
+
+# Version numbering follows the conventions of Semantic Versioning 2.0.0:
+# 1. MAJOR - changes with backwards-incompatible modifications to the API
+# 2. MINOR - for backwards-compatible additions to functionality
+# 3. PATCH - for backwards-compatible bug fixes
+# Source: http://semver.org/spec/v2.0.0.html
+MAJOR = 0
+MINOR = 0
+PATCH = 0
+__version__ = '%d.%d.%d' % (MAJOR, MINOR, PATCH)
