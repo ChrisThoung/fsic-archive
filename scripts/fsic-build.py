85421a3f7aa0df9e6640c641489cf981e07a0687
Chris Thoung <chris.thoung@gmail.com>
2014-07-26 00:19:51 +0100

    Add code-translation to fsic-build.py
    Add code to scripts/fsic-build.py to read Markdown files and generate
    class code.

--- scripts/fsic-build.py
+++ scripts/fsic-build.py
@@ -8,7 +8,12 @@ FSIC script to generate models from user-defined specification files.
 
 
 import argparse
+
 from fsic import __version__ as version
+
+from fsic.parser.markdown import extract
+from fsic.parser.chunk import parse
+from fsic.parser.code import translate
 
 
 parser = argparse.ArgumentParser(
@@ -28,4 +33,14 @@ parser.add_argument(
 
 
 if __name__ == '__main__':
+    # Parse arguments
     args = parser.parse_args()
+    # Read in file by file and flatten code-block list
+    chunks = [extract(open(f, 'rt').read()) for f in args.files]
+    chunks = [i for c in chunks for i in c]
+    # Parse to a list of Dictionary objects
+    blocks = [parse(c) for c in chunks]
+    # Extract Python code only and translate
+    python_code = [b['code'] for b in blocks if 'python' in b['classes']]
+    python_code = '\n'.join(python_code)
+    python_code = translate(python_code)
