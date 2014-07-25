83a7581e2318f5285d8d3a69eaa7ea4108c89ac2
Chris Thoung <chris.thoung@gmail.com>
2014-07-25 22:09:19 +0100

    Add positional argument to build script
    New argument for parser object in fsic-build.py to list input files to
    specify a model.

--- scripts/fsic-build.py
+++ scripts/fsic-build.py
@@ -1,4 +1,10 @@
 #!python
+"""
+fsic-build
+==========
+FSIC script to generate models from user-defined specification files.
+
+"""
 
 
 import argparse
@@ -8,7 +14,17 @@ from fsic import __version__ as version
 parser = argparse.ArgumentParser(
     description='FSIC model builder.',
     fromfile_prefix_chars='@')
-parser.add_argument('-V', '--version', action='version', version=version)
+parser.add_argument(
+    '-V', '--version',
+    action='version',
+    version=version)
+
+parser.add_argument(
+    'files',
+    metavar='FILE',
+    type=str,
+    nargs='+',
+    help='list of files that define the model')
 
 
 if __name__ == '__main__':
