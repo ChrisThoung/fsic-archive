5287662d062db958392bf1455d8fa87259de185c
Chris Thoung <chris.thoung@gmail.com>
2014-08-03 10:04:43 +0100

    Add with_prefix() function to ini module
    Add with_prefix() function to ini module, to add '[DEFAULT]' as a
    section header if missing from a configuration string.
    
    Add corresponding tests to test_ini.py.

--- fsic/parser/ini.py
+++ fsic/parser/ini.py
@@ -5,3 +5,26 @@ ini
 FSIC parser to handle INI-style configuration file strings.
 
 """
+
+
+def with_prefix(input, prefix='[DEFAULT]\n'):
+    """Prefix `input` with `prefix`, if not already present.
+
+    Parameters
+    ==========
+    input : string
+        Configuration data to modify
+    prefix : string
+        Prefix to test for and add, if necessary
+
+    Returns
+    =======
+    output : string
+        Copy of `input`, with `prefix` added if necessary.
+
+    """
+    if input.strip().startswith(prefix):
+        output = input
+    else:
+        output = prefix + input
+    return output
--- fsic/parser/tests/test_ini.py
+++ fsic/parser/tests/test_ini.py
@@ -2,3 +2,23 @@
 
 
 import fsic.parser.ini
+
+
+def test_with_prefix_no_change():
+    ini = '\n'.join([
+        '[DEFAULT]',
+        'NAME = SIM',
+        'DESCRIPTION = The simplest model with government money',])
+    assert fsic.parser.ini.with_prefix(ini) == ini
+
+
+def test_with_prefix_change():
+    ini = '\n'.join([
+        'NAME = SIM',
+        'DESCRIPTION = The simplest model with government money',])
+    assert fsic.parser.ini.with_prefix(ini) == '[DEFAULT]\n' + ini
+
+
+if __name__ == '__main__':
+    import nose
+    nose.runmodule()
