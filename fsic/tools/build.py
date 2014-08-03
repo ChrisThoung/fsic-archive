eb10e79862a1ca8cde2b619dd603d4e3144f5d12
Chris Thoung <chris.thoung@gmail.com>
2014-08-03 22:41:33 +0100

    Implement read_file() and read_string()
    Implement read_file() and read_string() functions to store the chunks of
    one or more Markdown files/strings.

--- fsic/tools/build.py
+++ fsic/tools/build.py
@@ -18,13 +18,43 @@ class Build:
     """
 
     def __init__(self):
-        self.model_template = os.path.join(
+        template_path = os.path.join(
             os.path.dirname(os.path.dirname(__file__)),
-            'templates',
-            'model.py')
+            'templates')
+        self.model_template = os.path.join(template_path, 'model.py')
+        self.linker_template = os.path.join(template_path, 'linker.py')
+        self.chunks = []
 
     def read_file(self, path):
-        pass
+        """Read in the chunks from the file in `path`.
+
+        Parameters
+        ==========
+        path : string
+            Location of file to parse, containing Markdown-formatted code
+
+        Notes
+        =====
+        This function reads the contents of `path` and passes them to
+        read_string() for processing.
+
+        """
+        with open(path, 'rt') as f:
+            self.read_string(f.read())
 
     def read_string(self, script):
-        pass
+        """Extract and store the chunks contained in the string `script`.
+
+        Parameters
+        ==========
+        script : string
+            Markdown-formatted string to parse
+
+        Notes
+        =====
+        This function extracts the chunks defined in `script` and *appends*
+        them to the variable `chunks`
+
+        """
+        from fsic.parser.markdown import extract
+        self.chunks = self.chunks + extract(script)
