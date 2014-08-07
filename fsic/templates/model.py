5e07bbe47bb008b202cfcdb92a40ec848bd4ace5
Chris Thoung <chris.thoung@gmail.com>
2014-08-07 21:27:35 +0100

    More work on Model template
    New initialise() function, including docstring.
    
    Add docstring to solve_equations() function.
    
    Early work on command-line argument parser.

--- fsic/templates/model.py
+++ fsic/templates/model.py
@@ -6,6 +6,13 @@ ___MODULE_DOCSTRING___
 """
 
 
+import argparse
+
+import numpy as np
+from pandas import Series, DataFrame
+
+from fsic import __version__ as version
+
 from fsic.model.model import Model
 
 
@@ -23,9 +30,49 @@ class ___MODEL___(Model):
 
     """
 
+    def initialise(self, span, default):
+        """Initialise the model for solution.
+
+        Parameters
+        ==========
+        span : list
+            The index to set the span of the model (used as the index for
+            individual variable Series objects)
+        default : float
+            Value to initialise variable Series objects with
+
+        """
+        ___INITIALISE___
+
     def solve_equations(self, period):
+        """Solve the model equations for `period`.
+
+        Parameters
+        ==========
+        period : Series index
+            The identifier of the period to solve
+
+        """
         ___SOLVE_EQUATIONS___
 
 
+parser = argparse.ArgumentParser(
+    description='___SHORT_DESCRIPTION___.',
+    fromfile_prefix_chars='@')
+parser.add_argument(
+    '-V', '--version',
+    action='version',
+    version=version)
+
+parser.add_argument(
+    '-o', '--output',
+    nargs='+',
+    metavar='OUTPUT',
+    default=None,
+    type=str,
+    required=False,
+    help='list of output files for model results')
+
+
 if __name__ == '__main__' and get_ipython() == None:
-    pass
+    args = parser.parse_args()
