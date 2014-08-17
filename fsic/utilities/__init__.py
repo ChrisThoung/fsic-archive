ea788f8a283980bff284d2ba74548378d8005c2d
Chris Thoung <chris.thoung@gmail.com>
2014-08-18 00:11:45 +0100

    Add documentation to utilities/__init__.py
    Add documentation to utilities/__init__.py, including brief description
    of `string` module.

---
+++ fsic/utilities/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+utilities
+=========
+FSIC subpackage of various utility functions.
+
+The subpackage contains the following module:
+
+* `string`, containing various functions to operate on strings for insertion
+  into code templates
+
+"""
