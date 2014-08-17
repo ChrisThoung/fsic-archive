fbe8181657cb92f016eeb422e16eac6e5bbeafbc
Chris Thoung <chris.thoung@gmail.com>
2014-08-17 23:47:18 +0100

    Add documentation to __init__.py files
    Add documentation to the __init__.py files of the following subpackages:
    
    * fsic.model
    * fsic.optimise
    * fsic.tools

---
+++ fsic/tools/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+tools
+=====
+FSIC subpackage of tools to generate models and supporting code.
+
+The subpackage contains the following module:
+
+* `build`, which defines the `Build` class, to generate a Python script for a
+  macroeconomic model
+
+"""
