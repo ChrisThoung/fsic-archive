d2eae719affeddbcf48b0b3ebb55873587f0f250
Chris Thoung <chris.thoung@gmail.com>
2014-08-21 21:02:20 +0100

    Add SIM equations to `optimise.order` test
    New (currently failing) test of `optimise.order.recursive()` function,
    based on equations of Godley and Lavoie (2007) Model SIM (Chapter 3).

--- fsic/optimise/tests/test_order.py
+++ fsic/optimise/tests/test_order.py
@@ -4,6 +4,25 @@
 import fsic.optimise.order
 
 
+def test_recursive():
+    equations = [
+        'self.H_h[period] = self.H_s[period]',
+        'self.C_d[period] = self.alpha_1[period] * self.YD[period] + self.alpha_2[period] * self.H_h[period-1]',
+        'self.C_s[period] = self.C_d[period]',
+        'self.G_s[period] = self.G_d[period]',
+        'self.T_s[period] = self.T_d[period]',
+        'self.N_s[period] = self.N_d[period]',
+        'self.H_s[period] = self.H_s[period-1] + self.G_d[period] - self.T_d[period]',
+        'self.H_h[period] = self.H_h[period-1] + self.YD[period] - self.C_d[period]',
+        'self.Y[period] = self.C_s[period] + self.G_s[period]',
+        'self.N_d[period] = self.Y[period] / self.W[period]',
+        'self.YD[period] = self.W[period] * self.N_s[period] - self.T_s[period]',
+        'self.T_d[period] = self.theta[period] * self.W[period] * self.N_s[period]',
+    ]
+    reordered = []
+    assert fsic.optimise.order.recursive(equations) == reordered
+
+
 if __name__ == '__main__':
     import nose
     nose.runmodule()
