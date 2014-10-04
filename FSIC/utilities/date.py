c65ed15fd128421186b619861b7f9031e816209c
Chris Thoung <chris.thoung@gmail.com>
2014-10-04 21:24:31 +0100

    Implement DateParser parse code
    Implement code and tests for date-parsing functions of new `DateParser`
    object. Still to implement conversion to a `datetime` object.

--- FSIC/utilities/date.py
+++ FSIC/utilities/date.py
@@ -5,3 +5,73 @@ date
 FSIC utility functions to handle time indexes.
 
 """
+
+
+import datetime
+import re
+
+from pandas import DatetimeIndex
+
+
+class DateParser:
+    """FSIC class to process period strings such as '2014Q1'."""
+
+    def __init__(self, date):
+        """Constructor: parse `date`.
+
+        Parameters
+        ==========
+        date : string
+            Date string to parse
+
+        Returns
+        =======
+        N/A
+
+        """
+        # Define the mapping from period string to number of periods
+        self.freq_map = {
+            'A': 1,
+            'Q': 4,
+            'M': 12,
+        }
+        # Parse `date`
+        self.parse_date(date)
+
+    def parse_date(self, date):
+        """Parse `date`.
+
+        Parameters
+        ==========
+        date : string
+            Date string to parse
+
+        Returns
+        =======
+        N/A
+
+        """
+        # Single year: convert to integer and fill in other metadata
+        try:
+            self.year = int(date)
+            self.period = 1
+            self.freq = 1
+            self.freq_id = 'A'
+        # Otherwise, parse information by field
+        except:
+            # Apply regular expression
+            pattern = re.compile(
+                r'''
+                (\d+)       # Year: One or more digits
+                ([AQM])     # Periodicity: One character
+                (\d+)       # Period: One or more digits
+                ''',
+                re.VERBOSE)
+            match = pattern.match(date)
+            # Split out fields
+            self.year, self.freq_id, self.period = match.groups()
+            # Convert year and period to integers
+            self.year = int(self.year)
+            self.period = int(self.period)
+            # Identify number of periods from `freq_map`
+            self.freq = self.freq_map[self.freq_id]
--- FSIC/utilities/tests/test_date.py
+++ FSIC/utilities/tests/test_date.py
@@ -1,7 +1,28 @@
 # -*- coding: utf-8 -*-
 
 
-import FSIC.utilities.date
+from FSIC.utilities.date import DateParser
+
+
+def test_parse():
+    test_cases = {
+        '2000': [2000, 1, 1, 'A'],
+        '2000A1': [2000, 1, 1, 'A'],
+        '2000A01': [2000, 1, 1, 'A'],
+
+        '2005Q2': [2005, 2, 4, 'Q'],
+        '2005Q02': [2005, 2, 4, 'Q'],
+
+        '2010M9': [2010, 9, 12, 'M'],
+        '2010M09': [2010, 9, 12, 'M'],
+    }
+    for case, expected in test_cases.items():
+        parser = DateParser(case)
+        result = [parser.year,
+                  parser.period,
+                  parser.freq,
+                  parser.freq_id]
+        assert result == expected
 
 
 if __name__ == '__main__':
