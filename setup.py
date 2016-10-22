8824c08f0a8f617cea6b19234a082921a862f131
Chris Thoung <chris.thoung@gmail.com>
2016-10-22 22:19:18 +0100

    BLD: Drop Python 3.6 from CI for now
    Not yet supported by Travis.

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ Programming Language :: Python :: 3
 Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
-Programming Language :: Python :: 3.6
 Topic :: Scientific/Engineering
 '''.split('\n') if len(c.strip())]
 
